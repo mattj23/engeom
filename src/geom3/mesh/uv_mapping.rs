9fcc0a9272b3d3a7bddeceac7dad978185e642bb
Matt Jarvis <mattj23@gmail.com>
2025-02-11 20:47:24 -0500

    2d uv mapping

--- src/geom3/mesh/uv_mapping.rs
+++ src/geom3/mesh/uv_mapping.rs
@@ -1,42 +1,23 @@
 //! This module contains an abstraction for mapping triangles in a mesh to a 2D UV space.
 
+use parry2d_f64::query::PointQueryWithLocation;
 use crate::geom2::Point2;
 use crate::{Result, To3D};
-use parry3d_f64::query::PointQueryWithLocation;
-use parry3d_f64::shape::TriMesh;
+use parry2d_f64::shape::TriMesh;
 
 #[derive(Clone)]
 pub struct UvMapping {
-    triangles: Vec<[Point2; 3]>,
     tri_map: TriMesh,
 }
 
 impl UvMapping {
-    pub fn new(triangles: Vec<[Point2; 3]>) -> Result<Self> {
-        let tri_map = tri_map_from_triangles(&triangles)?;
-        Ok(Self { triangles, tri_map })
+    pub fn new(vertices: Vec<Point2>, faces: Vec<[u32; 3]>) -> Result<Self> {
+        let tri_map = TriMesh::new(vertices, faces)?;
+        Ok(Self { tri_map })
     }
 
-    pub fn new_from_vertices(vertices: &[Point2]) -> Result<Self> {
-        let mut triangles = Vec::new();
-        for tri in vertices.chunks_exact(3) {
-            triangles.push([tri[0], tri[1], tri[2]]);
-        }
-        Self::new(triangles)
-    }
-
-    pub fn to_vertices(&self) -> Vec<Point2> {
-        let mut vertices = Vec::new();
-        for tri in self.triangles() {
-            vertices.push(tri[0]);
-            vertices.push(tri[1]);
-            vertices.push(tri[2]);
-        }
-        vertices
-    }
-
-    pub fn triangles(&self) -> &[[Point2; 3]] {
-        &self.triangles
+    pub fn faces(&self) -> &[[u32; 3]] {
+        &self.tri_map.indices()
     }
 
     /// Given a triangle ID and a barycentric coordinate, return the corresponding point in the
@@ -71,21 +52,4 @@ impl UvMapping {
         let (_, (t_id, loc)) = result;
         Some((t_id as usize, loc.barycentric_coordinates().unwrap()))
     }
-}
-
-fn tri_map_from_triangles(tris: &[[Point2; 3]]) -> Result<TriMesh> {
-    let mut vertices = Vec::new();
-    let mut triangles: Vec<[u32; 3]> = Vec::new();
-
-    for tri in tris {
-        let i = vertices.len() as u32;
-        vertices.push(tri[0].to_3d());
-        vertices.push(tri[1].to_3d());
-        vertices.push(tri[2].to_3d());
-
-        triangles.push([i, i + 1, i + 2]);
-    }
-
-    TriMesh::new(vertices, triangles)
-        .map_err(|e| format!("Failed to create TriMesh from triangles: {:?}", e).into())
 }
