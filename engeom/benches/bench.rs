4402f75a2a08f5ec67f26137ea63dcfd99d99faf
Matt Jarvis <mattj23@gmail.com>
2025-07-12 12:23:40 -0400

    vec bool based index mask benchmark

--- engeom/benches/bench.rs
+++ engeom/benches/bench.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 use engeom::common::IndexMask;
 use divan::{Bencher, black_box};
 
-const N: usize = 1_000_000;
+const N: usize = 10_000_000;
 
 fn main() {
     // Run registered benchmarks.
@@ -13,20 +13,20 @@ fn main() {
 #[divan::bench]
 fn index_mask_set(bencher: Bencher) {
     let to_set = (0..N).filter(|i| i % 2 == 0).collect::<Vec<_>>();
-    let mut mask = HashSet::<usize>::new();
+    let mut mask = IndexMask::new(N, false);
 
     bencher.bench_local(move || {
         for x in to_set.iter() {
-            black_box(&mut mask).insert(*x);
+            black_box(&mut mask).set(*x, true);
         }
     });
 }
 
-fn prep_mask(m: usize) -> HashSet<usize> {
+fn prep_mask(m: usize) -> IndexMask {
     let to_set = (0..N).filter(|i| i % m == 0).collect::<Vec<_>>();
-    let mut mask = HashSet::<usize>::new();
+    let mut mask = IndexMask::new(N, false);
     for x in to_set.iter() {
-        mask.insert(*x);
+        mask.set(*x, true);
     }
 
     mask
@@ -37,7 +37,7 @@ fn index_mask_get(bencher: Bencher) {
     let mask = prep_mask(2);
     bencher.bench_local(move || {
         for i in 0..N {
-            let x = black_box(&mask).contains(&i);
+            let x = black_box(&mask).get(i);
         }
     });
 }
@@ -47,9 +47,6 @@ fn index_mask_to_vec(bencher: Bencher) {
     let mask = prep_mask(2);
 
     bencher.bench_local(move || {
-        let mut v = Vec::new();
-        for i in black_box(&mask).iter() {
-            v.push(black_box(*i));
-        }
+        let v = black_box(&mask).to_indices();
     });
 }
