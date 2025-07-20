edd57834124dd163b023a09b56eb2fd65e7f9237
Matt Jarvis <mattj23@gmail.com>
2025-07-20 01:56:41 -0400

    parallelized ball rolling

--- engeom/src/raster2/ball_rolling.rs
+++ engeom/src/raster2/ball_rolling.rs
@@ -1,16 +1,10 @@
 //! Implementation of a ball rolling background algorithm for scalar raster data.  This algorithm
 //! is based on the widely known algorithm first introduced in a 1983 paper by Stanley Sternberg.
 
-use crate::image::Luma;
 use crate::na::DMatrix;
-use crate::raster2::roi::RoiOverlay;
-use crate::raster2::{
-    Point2I, Point2IIndexAccess, RasterRoi, ScalarImage, ScalarRaster, SizeForIndex, Vector2I,
-};
+use crate::raster2::{Point2IIndexAccess, ScalarRaster, SizeForIndex};
 use crate::{Point2, Result};
-use colorgrad::preset::turbo;
-use imageproc::definitions::Image;
-use std::path::Path;
+use rayon::prelude::*;
 
 /// This function performs a ball rolling algorithm to compute a background for a raster of
 /// scalar values. The algorithm was first introduced by Stanley Sternberg in 1983 and is widely
@@ -46,41 +40,61 @@ pub fn ball_rolling_background(raster: &ScalarRaster, radius: f64) -> Result<Sca
             matrix.set_at(p, f64::NEG_INFINITY)?
         }
     }
+    let tail_n = (ball.nrows() - 1) as i32 / 2;
+
+    let xi_work = (-tail_n..(raster.width() as i32 + tail_n)).collect::<Vec<_>>();
+
+    let collected = xi_work
+        .par_chunks(xi_work.len() / rayon::current_num_threads())
+        .map(|slice| {
+            let mut rolled = DMatrix::zeros(matrix.nrows(), matrix.ncols());
+            rolled.fill(f64::INFINITY);
+
+            for xi in slice {
+                for yi in -tail_n..(raster.height() as i32 + tail_n) {
+                    let (min_mi, min_ki, count_ki) = wv(yi, matrix.nrows(), tail_n);
+                    let (min_mj, min_kj, count_kj) = wv(*xi, matrix.ncols(), tail_n);
+
+                    let mut contact_height = f64::INFINITY;
+
+                    for j in 0..count_kj {
+                        for i in 0..count_ki {
+                            let ki = min_ki + i;
+                            let kj = min_kj + j;
+                            let mi = min_mi + i;
+                            let mj = min_mj + j;
+                            contact_height = contact_height.min(ball[(ki, kj)] - matrix[(mi, mj)]);
+                        }
+                    }
+
+                    if !contact_height.is_finite() {
+                        continue;
+                    }
+
+                    for j in 0..count_kj {
+                        for i in 0..count_ki {
+                            let ki = min_ki + i;
+                            let kj = min_kj + j;
+                            let mi = min_mi + i;
+                            let mj = min_mj + j;
+                            rolled[(mi, mj)] =
+                                rolled[(mi, mj)].min(ball[(ki, kj)] - contact_height);
+                        }
+                    }
+                }
+            }
+
+            rolled
+        })
+        .collect::<Vec<_>>();
 
     let mut rolled = DMatrix::zeros(matrix.nrows(), matrix.ncols());
     rolled.fill(f64::INFINITY);
 
-    let tail_n = (ball.nrows() - 1) as i32 / 2;
-
-    for xi in -tail_n..(raster.width() as i32 + tail_n) {
-        for yi in -tail_n..(raster.height() as i32 + tail_n) {
-            let (min_mi, min_ki, count_ki) = wv(yi, matrix.nrows(), tail_n);
-            let (min_mj, min_kj, count_kj) = wv(xi, matrix.ncols(), tail_n);
-
-            let mut contact_height = f64::INFINITY;
-
-            for j in 0..count_kj {
-                for i in 0..count_ki {
-                    let ki = min_ki + i;
-                    let kj = min_kj + j;
-                    let mi = min_mi + i;
-                    let mj = min_mj + j;
-                    contact_height = contact_height.min(ball[(ki, kj)] - matrix[(mi, mj)]);
-                }
-            }
-
-            if !contact_height.is_finite() {
-                continue;
-            }
-
-            for j in 0..count_kj {
-                for i in 0..count_ki {
-                    let ki = min_ki + i;
-                    let kj = min_kj + j;
-                    let mi = min_mi + i;
-                    let mj = min_mj + j;
-                    rolled[(mi, mj)] = rolled[(mi, mj)].min(ball[(ki, kj)] - contact_height);
-                }
+    for r in collected {
+        for j in 0..r.nrows() {
+            for i in 0..r.ncols() {
+                rolled[(j, i)] = rolled[(j, i)].min(r[(j, i)]);
             }
         }
     }
