bcab0997c1d07dfa73de636f2efc1a42c61ef2c7
Matt Jarvis <mattj23@gmail.com>
2025-07-20 00:46:49 -0400

    u16 implementation is slower

--- engeom/src/raster2/ball_rolling.rs
+++ engeom/src/raster2/ball_rolling.rs
@@ -1,13 +1,16 @@
 //! Implementation of a ball rolling background algorithm for scalar raster data.  This algorithm
 //! is based on the widely known algorithm first introduced in a 1983 paper by Stanley Sternberg.
 
-use std::path::Path;
-use colorgrad::preset::turbo;
 use crate::na::DMatrix;
 use crate::raster2::roi::RoiOverlay;
-use crate::raster2::{Point2I, Point2IIndexAccess, RasterRoi, ScalarImage, ScalarRaster, SizeForIndex, Vector2I};
+use crate::raster2::{
+    Point2I, Point2IIndexAccess, RasterRoi, ScalarImage, ScalarRaster, SizeForIndex, Vector2I,
+};
 use crate::{Point2, Result};
+use colorgrad::preset::turbo;
 use imageproc::definitions::Image;
+use std::path::Path;
+use crate::image::Luma;
 
 /// This function performs a ball rolling algorithm to compute a background for a raster of
 /// scalar values. The algorithm was first introduced by Stanley Sternberg in 1983 and is widely
@@ -37,107 +40,68 @@ use imageproc::definitions::Image;
 /// ```
 pub fn ball_rolling_background(raster: &ScalarRaster, radius: f64) -> Result<ScalarRaster> {
     let ball = ball_buffer(radius, &raster);
-    ball.render_with_cmap(&Path::new("D:/temp/k/ball.png"), &turbo(), Some((-2.0, 2.0)))?;
+    ball.render_with_cmap(
+        &Path::new("D:/temp/k/ball.png"),
+        &turbo(),
+        Some((-2.0, 2.0)),
+    )?;
 
     // The mask will be the same as the original raster, so we only need to create a buffer to
     // hold the hull values
     // let mut hull = ScalarImage::new(raster.width(), raster.height());
-    let mut hull = ScalarRaster::filled_like(&raster, u16::MAX);
+    let mut hull = ScalarImage::new(raster.width(), raster.height());
+    hull.fill(u16::MAX);
 
     let tail_n = (ball.width() - 1) as i32 / 2;
-    let tail_v = Vector2I::new(tail_n, tail_n);
-    let full_roi = RasterRoi::new(
-        Point2I::origin(),
-        Point2I::new(raster.width() as i32, raster.height() as i32),
-    );
 
-    for p in ExpandedIter::from_raster(&raster, tail_n) {
-        let roi = RasterRoi::new(p - tail_v, p + tail_v);
-        let overlay = RoiOverlay::new(roi, full_roi);
+    for xi in -tail_n..(hull.width() as i32 + tail_n) {
+        for yi in -tail_n..(hull.height() as i32 + tail_n) {
+            let mut contact_height = u16::MAX;
 
-        let mut contact_height = u16::MAX;
+            let (min_mi, min_ki, count_ki) = window_vals(yi, hull.height(), tail_n);
+            let (min_mj, min_kj, count_kj) = window_vals(xi, hull.width(), tail_n);
 
-        for q in overlay.iter_intersection_a() {
-            if let (Some(b), Some(m)) = (ball.u_at(q.local), raster.u_at(q.parent)) {
-                contact_height = contact_height.min(b - m);
+            for j in 0..count_kj {
+                for i in 0..count_ki {
+                    let ki = min_ki + i;
+                    let kj = min_kj + j;
+                    let mi = min_mi + i;
+                    let mj = min_mj + j;
+
+                    if ball.mask.buffer.get_pixel(kj, ki)[0] == 255
+                        && raster.mask.buffer.get_pixel(mj, mi)[0] == 255
+                    {
+                        let b = ball.values.get_pixel(kj, ki)[0];
+                        let m = raster.values.get_pixel(mj, mi)[0];
+                        contact_height = contact_height.min(b - m)
+                    }
+                }
+            }
+
+            if contact_height == u16::MAX {
+                continue; // No contact, skip this point
+            }
+
+            for j in 0..count_kj {
+                for i in 0..count_ki {
+                    let ki = min_ki + i;
+                    let kj = min_kj + j;
+                    let mi = min_mi + i;
+                    let mj = min_mj + j;
+
+                    if ball.mask.buffer.get_pixel(kj, ki)[0] == 255
+                        && raster.mask.buffer.get_pixel(mj, mi)[0] == 255
+                    {
+                        let b = ball.values.get_pixel(kj, ki)[0];
+                        let v = hull.get_pixel(mj, mi)[0];
+                        hull.put_pixel(mj, mi, Luma([v.min(b - contact_height)]));
+                    }
+                }
             }
         }
-
-        for q in overlay.iter_intersection_a() {
-            if let (Some(b), Some(v)) = (ball.u_at(q.local), hull.u_at(q.parent)) {
-                hull.set_u_at(q.parent, Some(v.min(b - contact_height)))?;
-            }
-        }
     }
 
-    Ok(hull)
-}
-
-struct ExpandedIter {
-    min_x: i32,
-    min_y: i32,
-    max_x: i32,
-    max_y: i32,
-    current_x: i32,
-    current_y: i32,
-}
-
-impl ExpandedIter {
-    fn from_raster(raster: &ScalarRaster, expand: i32) -> Self {
-        Self::new(
-            -expand,
-            -expand,
-            raster.width() as i32 + expand - 1,
-            raster.height() as i32 + expand - 1,
-        )
-    }
-    fn new(min_x: i32, min_y: i32, max_x: i32, max_y: i32) -> Self {
-        Self {
-            min_x,
-            min_y,
-            max_x,
-            max_y,
-            current_x: min_x,
-            current_y: min_y,
-        }
-    }
-}
-
-impl Iterator for ExpandedIter {
-    type Item = Point2I;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.current_y > self.max_y {
-            return None;
-        }
-
-        let point = Point2I::new(self.current_x, self.current_y);
-
-        // Move to the next point
-        self.current_x += 1;
-        if self.current_x > self.max_x {
-            self.current_x = self.min_x;
-            self.current_y += 1;
-        }
-
-        Some(point)
-    }
-}
-
-fn indices(
-    i: usize,
-    j: usize,
-    min_mi: usize,
-    min_ki: usize,
-    min_mj: usize,
-    min_kj: usize,
-) -> (usize, usize, usize, usize) {
-    let ki = min_ki + i;
-    let kj = min_kj + j;
-    let mi = min_mi + i;
-    let mj = min_mj + j;
-
-    (ki, kj, mi, mj)
+    ScalarRaster::try_new(hull, raster.mask.clone(), raster.px_size, raster.min_z, raster.max_z)
 }
 
 fn ball_buffer(radius: f64, target: &ScalarRaster) -> ScalarRaster {
@@ -160,11 +124,11 @@ fn ball_buffer(radius: f64, target: &ScalarRaster) -> ScalarRaster {
     ScalarRaster::from_matrix(&matrix, target.px_size, target.min_z, target.max_z)
 }
 
-fn window_vals(a: i32, count: usize, tail_n: i32) -> (usize, usize, usize) {
+fn window_vals(a: i32, count: u32, tail_n: i32) -> (u32, u32, u32) {
     let min_mi = (a - tail_n).max(0);
     let min_ki = min_mi + tail_n - a;
     let max_mi = (a + tail_n).min(count as i32 - 1);
     let count_ki = max_mi - min_mi + 1;
 
-    (min_mi as usize, min_ki as usize, count_ki as usize)
+    (min_mi as u32, min_ki as u32, count_ki as u32)
 }
