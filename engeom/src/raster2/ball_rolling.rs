7c17024b2d20eca65e569861ef054c1a13d9d3e5
Matt Jarvis <mattj23@gmail.com>
2025-07-20 01:10:55 -0400

    f64 implementation without branching

--- engeom/src/raster2/ball_rolling.rs
+++ engeom/src/raster2/ball_rolling.rs
@@ -1,6 +1,7 @@
 //! Implementation of a ball rolling background algorithm for scalar raster data.  This algorithm
 //! is based on the widely known algorithm first introduced in a 1983 paper by Stanley Sternberg.
 
+use crate::image::Luma;
 use crate::na::DMatrix;
 use crate::raster2::roi::RoiOverlay;
 use crate::raster2::{
@@ -10,7 +11,6 @@ use crate::{Point2, Result};
 use colorgrad::preset::turbo;
 use imageproc::definitions::Image;
 use std::path::Path;
-use crate::image::Luma;
 
 /// This function performs a ball rolling algorithm to compute a background for a raster of
 /// scalar values. The algorithm was first introduced by Stanley Sternberg in 1983 and is widely
@@ -39,27 +39,25 @@ use crate::image::Luma;
 ///
 /// ```
 pub fn ball_rolling_background(raster: &ScalarRaster, radius: f64) -> Result<ScalarRaster> {
-    let ball = ball_buffer(radius, &raster);
-    ball.render_with_cmap(
-        &Path::new("D:/temp/k/ball.png"),
-        &turbo(),
-        Some((-2.0, 2.0)),
-    )?;
+    let ball = ball_matrix(radius, raster.px_size);
+    let mut matrix = raster.to_matrix();
+    for p in matrix.iter_indices() {
+        if !raster.mask.get_point(p) {
+            matrix.set_at(p, f64::NEG_INFINITY)?
+        }
+    }
 
-    // The mask will be the same as the original raster, so we only need to create a buffer to
-    // hold the hull values
-    // let mut hull = ScalarImage::new(raster.width(), raster.height());
-    let mut hull = ScalarImage::new(raster.width(), raster.height());
-    hull.fill(u16::MAX);
+    let mut rolled = DMatrix::zeros(matrix.nrows(), matrix.ncols());
+    rolled.fill(f64::INFINITY);
 
-    let tail_n = (ball.width() - 1) as i32 / 2;
+    let tail_n = (ball.nrows() - 1) as i32 / 2;
 
-    for xi in -tail_n..(hull.width() as i32 + tail_n) {
-        for yi in -tail_n..(hull.height() as i32 + tail_n) {
-            let mut contact_height = u16::MAX;
+    for xi in -tail_n..(raster.width() as i32 + tail_n) {
+        for yi in -tail_n..(raster.height() as i32 + tail_n) {
+            let (min_mi, min_ki, count_ki) = wv(yi, matrix.nrows(), tail_n);
+            let (min_mj, min_kj, count_kj) = wv(xi, matrix.ncols(), tail_n);
 
-            let (min_mi, min_ki, count_ki) = window_vals(yi, hull.height(), tail_n);
-            let (min_mj, min_kj, count_kj) = window_vals(xi, hull.width(), tail_n);
+            let mut contact_height = f64::INFINITY;
 
             for j in 0..count_kj {
                 for i in 0..count_ki {
@@ -67,19 +65,12 @@ pub fn ball_rolling_background(raster: &ScalarRaster, radius: f64) -> Result<Sca
                     let kj = min_kj + j;
                     let mi = min_mi + i;
                     let mj = min_mj + j;
-
-                    if ball.mask.buffer.get_pixel(kj, ki)[0] == 255
-                        && raster.mask.buffer.get_pixel(mj, mi)[0] == 255
-                    {
-                        let b = ball.values.get_pixel(kj, ki)[0];
-                        let m = raster.values.get_pixel(mj, mi)[0];
-                        contact_height = contact_height.min(b - m)
-                    }
+                    contact_height = contact_height.min(ball[(ki, kj)] - matrix[(mi, mj)]);
                 }
             }
 
-            if contact_height == u16::MAX {
-                continue; // No contact, skip this point
+            if !contact_height.is_finite() {
+                continue;
             }
 
             for j in 0..count_kj {
@@ -88,30 +79,34 @@ pub fn ball_rolling_background(raster: &ScalarRaster, radius: f64) -> Result<Sca
                     let kj = min_kj + j;
                     let mi = min_mi + i;
                     let mj = min_mj + j;
-
-                    if ball.mask.buffer.get_pixel(kj, ki)[0] == 255
-                        && raster.mask.buffer.get_pixel(mj, mi)[0] == 255
-                    {
-                        let b = ball.values.get_pixel(kj, ki)[0];
-                        let v = hull.get_pixel(mj, mi)[0];
-                        hull.put_pixel(mj, mi, Luma([v.min(b - contact_height)]));
-                    }
+                    rolled[(mi, mj)] = rolled[(mi, mj)].min(ball[(ki, kj)] - contact_height);
                 }
             }
         }
     }
 
-    ScalarRaster::try_new(hull, raster.mask.clone(), raster.px_size, raster.min_z, raster.max_z)
+    for p in rolled.iter_indices() {
+        if !raster.mask.get_point(p) {
+            rolled.set_at(p, f64::NAN)?;
+        }
+    }
+
+    Ok(ScalarRaster::from_matrix(
+        &rolled,
+        raster.px_size,
+        raster.min_z,
+        raster.max_z,
+    ))
 }
 
-fn ball_buffer(radius: f64, target: &ScalarRaster) -> ScalarRaster {
-    let px_dia = (2.0 * radius / target.px_size).ceil() as usize | 1;
+fn ball_matrix(radius: f64, px_size: f64) -> DMatrix<f64> {
+    let px_dia = (2.0 * radius / px_size).ceil() as usize | 1;
 
     let mut matrix = DMatrix::zeros(px_dia, px_dia);
     let c = Point2::new(radius, radius);
 
     for p in matrix.iter_indices() {
-        let a = Point2::new(p.x as f64, p.y as f64) * target.px_size;
+        let a = Point2::new(p.x as f64, p.y as f64) * px_size;
         let b = (a - c).norm();
         let v = if b >= radius {
             f64::INFINITY
@@ -121,14 +116,14 @@ fn ball_buffer(radius: f64, target: &ScalarRaster) -> ScalarRaster {
         matrix.set_at(p, v).unwrap()
     }
 
-    ScalarRaster::from_matrix(&matrix, target.px_size, target.min_z, target.max_z)
+    matrix
 }
 
-fn window_vals(a: i32, count: u32, tail_n: i32) -> (u32, u32, u32) {
+fn wv(a: i32, count: usize, tail_n: i32) -> (usize, usize, usize) {
     let min_mi = (a - tail_n).max(0);
     let min_ki = min_mi + tail_n - a;
     let max_mi = (a + tail_n).min(count as i32 - 1);
     let count_ki = max_mi - min_mi + 1;
 
-    (min_mi as u32, min_ki as u32, count_ki as u32)
+    (min_mi as usize, min_ki as usize, count_ki as usize)
 }
