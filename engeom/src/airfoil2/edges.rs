7b1b3fe2f87ceae77968b89504958702d928638b
Matt Jarvis <mattj23@gmail.com>
2026-06-11 17:40:02 -0400

    refactoring common edge finding tools

--- engeom/src/airfoil2/edges.rs
+++ engeom/src/airfoil2/edges.rs
@@ -2,7 +2,7 @@ use crate::airfoil2::inscribed::{Inscribed, InscribedVec};
 use crate::airfoil2::{AfEdge, AfEdgeGeometry, SectionInput};
 use crate::common::mid_point;
 use crate::geom2::{BndBuildFn, BoundaryData2, BoundaryEditor, fit_boundary_to_points};
-use crate::{DVector, Point2, Result};
+use crate::{DVector, Line2, Point2, Result};
 
 #[derive(Clone, Debug)]
 pub struct AfEdgeFit {
@@ -21,63 +21,21 @@ impl AfEdgeFit {
     }
 }
 
-pub fn edge_prep(
-    input: &SectionInput,
-    circles: Vec<Inscribed>,
-    at_front: bool,
-) -> Result<(InscribedVec, Vec<Point2>)> {
-    if circles.len() < 2 {
-        return Err("Not enough inscribed circles to partition edge geometry".into());
-    }
-
-    let mut stack = InscribedVec::new(circles);
-    // If we're supposed to be detecting the edge at the front of the airfoil, we'll reverse the
-    // stack of circles so that we're working at the back
-    if at_front {
-        stack.reverse_order_only();
-    }
-    let test_line = stack.end_clip_line()?;
-
-    // Now we want to isolate the data beyond the end of the last circle
-    let mut points = Vec::new();
-    for p in input.section.points() {
-        if test_line.scalar_project(p).is_sign_positive() {
-            points.push(*p);
-        }
-    }
-
-    Ok((stack, points))
-}
-
-pub fn edge_finalize(mut circles: InscribedVec, at_front: bool) -> Vec<Inscribed> {
-    if at_front {
-        circles.reverse_order_only();
-    }
-
-    circles.take_vec()
-}
-
 pub fn square_edge(
     input: &SectionInput,
     circles: Vec<Inscribed>,
     at_front: bool,
 ) -> Result<AfEdgeFit> {
     // TODO: Can we refine the stack of inscribed circles?
-    let (stack, fit_points) = edge_prep(input, circles, at_front)?;
-
-    let last = stack.last().unwrap();
-    let p0 = last.p0.clone();
-    let p1 = last.p1.clone();
+    let working = EdgeWork::new(input, circles, at_front)?;
+    let p0 = working.last()?.p0.clone();
+    let p1 = working.last()?.p1.clone();
 
     // To find the initial, we'll find how far the farthest point is from the end clipping line
     // and add that vector to p0 and p1
-    let l = stack.end_clip_line()?;
-    let d_max = fit_points
-        .iter()
-        .map(|p| l.scalar_project(p))
-        .fold(0.0f64, |a, b| a.max(b));
-    let i0 = p0 + l.direction * d_max;
-    let i1 = p1 + l.direction * d_max;
+    let d = working.clip.direction * working.clip_max_scalar();
+    let i0 = p0 + d;
+    let i1 = p1 + d;
     let initial = DVector::from(vec![i0.x, i0.y, i1.x, i1.y]);
 
     let builder: BndBuildFn = Box::new(move |params: &DVector| {
@@ -88,15 +46,93 @@ pub fn square_edge(
         bdata.try_to_boundary()
     });
 
-    let result = fit_boundary_to_points(&fit_points, &builder, initial, false)?;
+    let result = fit_boundary_to_points(&working.fit_points, &builder, initial, false)?;
     let corner0 = Point2::new(result.params[0], result.params[1]);
     let corner1 = Point2::new(result.params[2], result.params[3]);
     let point = mid_point(&corner0, &corner1);
 
-    let c = edge_finalize(stack, at_front);
+    let c = working.take_circles();
     let edge = AfEdge::new(point, AfEdgeGeometry::Square(corner0, corner1));
-
     Ok(AfEdgeFit::new(edge, result.residuals, c))
+}
+
+pub fn full_round_edge(
+    input: &SectionInput,
+    circles: Vec<Inscribed>,
+    at_front: bool,
+) -> Result<AfEdgeFit> {
+    // TODO: Can we refine the stack of inscribed circles?
+    let working = EdgeWork::new(input, circles, at_front)?;
+    let p0 = working.last()?.p0.clone();
+    let p1 = working.last()?.p1.clone();
+
+    todo!()
+}
+
+// =============================================================================================
+// Common tools for the edge implementations
+// =============================================================================================
+struct EdgeWork<'a> {
+    input: &'a SectionInput<'a>,
+    stack: InscribedVec,
+    fit_points: Vec<Point2>,
+    clip: Line2,
+    at_front: bool,
+}
+
+impl<'a> EdgeWork<'a> {
+    fn last(&self) -> Result<&Inscribed> {
+        self.stack
+            .last()
+            .ok_or_else(|| "No inscribed circles found".into())
+    }
+
+    /// The maximum scalar projection of the fit points on the clipping line
+    fn clip_max_scalar(&self) -> f64 {
+        let d_max = self
+            .fit_points
+            .iter()
+            .map(|p| self.clip.scalar_project(p))
+            .fold(0.0f64, |a, b| a.max(b));
+        d_max
+    }
+
+    fn new(input: &'a SectionInput<'a>, circles: Vec<Inscribed>, at_front: bool) -> Result<Self> {
+        if circles.len() < 2 {
+            return Err("Not enough inscribed circles to partition edge geometry".into());
+        }
+        let mut stack = InscribedVec::new(circles);
+        // If we're supposed to be detecting the edge at the front of the airfoil, we'll reverse the
+        // stack of circles so that we're working at the back
+        if at_front {
+            stack.reverse_order_only();
+        }
+        let clip = stack.end_clip_line()?;
+
+        // Now we want to isolate the data beyond the end of the last circle
+        let mut fit_points = Vec::new();
+        for p in input.section.points() {
+            if clip.scalar_project(p).is_sign_positive() {
+                fit_points.push(*p);
+            }
+        }
+
+        Ok(Self {
+            input,
+            stack,
+            fit_points,
+            clip,
+            at_front,
+        })
+    }
+
+    fn take_circles(self) -> Vec<Inscribed> {
+        let mut stack = self.stack;
+        if self.at_front {
+            stack.reverse_order_only();
+        }
+        stack.take_vec()
+    }
 }
 
 #[cfg(test)]
